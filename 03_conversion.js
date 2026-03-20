76412a6c8f3f18dbb0af470225269d621e59e1ff
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-03-20 21:30:42 +0530

    Add files via upload

---
+++ 03_conversion.js
@@ -0,0 +1,30 @@
+// let score = 23
+// console.log(typeof score)
+// console.log(typeof (score))
+// console.log(score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// console.log(typeof true)
+// console.log(typeof +true)
+// console.log(typeof false)
+// console.log(typeof +false)
+// console.log(typeof " ")
+// console.log(typeof "ritu")
+// console.log(typeof +"")
+
+// array, object functions
+const outsideTemp = null;
+const heros = ["shktimAN","NAAGDEVTA","MODIJI"]
+let myObj = {
+    name : "Raghu",
+    age: 21,
+}
+const myFunction = function(){
+    console.log("Namaste Duniya");
+}
+
+console.log (typeof outsideTemp);
+console.log(typeof myFunction);
---
+++ 03_conversionoperation.js
@@ -0,0 +1,49 @@
+// let score ="33abc"
+// let score ="gama"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber= Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true , 0 => false
+// "gama" => true
+
+// let someNumber = 33
+// let stringNumber =String(someNumber)
+// console.log(someNumber);
+// console.log(typeof (someNumber));
+
+// ************** Operations ******************
+
+// let value = 3
+// let neValue = -value
+// console.log(neValue);
+
+let str1= "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); because here primitive are number thats why 
+// they are behave as number hint../
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
