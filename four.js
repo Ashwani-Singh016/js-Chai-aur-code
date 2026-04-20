51002ac4bb9454ce6efd7bf8f61c82a95d6e3151
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-04-20 12:32:47 +0530

    Add files via upload

---
+++ four.js
@@ -0,0 +1,12 @@
+const myObject= {
+    js:'Javascript'
+    py:  "python"
+    rb:"ruby"
+}
+for (const key in myObject) {
+    // console.log(Object);   
+}
+const programming = ["js","python","cpp","java"]
+for (const key of programming) {
+    console.log(key);   
+}
