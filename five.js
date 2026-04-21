cadccc9d3bf43a1f90b83fad47f52760774ddf11
Ashwani-Singh016 <singhaswag1729@gmail.com>
2026-04-21 23:44:28 +0530

    Add files via upload

---
+++ five.js
@@ -0,0 +1,23 @@
+// const coding = ["js","python","cpp","java"]
+
+// coding.forEach(function (val){
+//     console.log(val);
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item);
+// }) 
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(printMe)
+coding.forEach((item,arr,index)=>{
+    console.log(item,index,arr);
+})
+
+const myCoding ={
+    languagename : "javascript"
+}
+
